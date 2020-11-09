3ed24a78b7b1400202fd05053f89da4391f4417f
unknown <marselthomas23@gmail.com>
2020-11-09 19:43:01 +0700

    exercise 7

---
+++ pertemuan23/index.js
@@ -0,0 +1,88 @@
+//Variable
+//let & const
+//let _umur =25;
+//let $nama ="Marchel";
+//console. log(_umur);
+//console. log($nama);
+
+//const tahunlahir = 2001;
+//console. log($nama);
+//console. log($nama);
+
+//let  alamat;
+//console. log(alamat);
+
+//const tahunLahir = 2001;
+//console. log(tahunLahir);
+
+//Operator
+
+//Arithmetic Operator (+,-,*,/)
+
+//let angka1 =10;
+//let angka2 =20;
+//console. log(angka1 + angka2);
+
+//let umur = 30;
+//let namaDepan = "John";
+//let namabelakang = "Doe";
+//console. log(
+ //   "Nama Saya " + 
+  //  namaDepan +
+  //  " " +
+   // namabelakang +
+   // ", Umur saya adalah " +
+   // umur +
+   // " tahun"
+
+//);
+//Operator perbandingan
+
+//let angka1 = 10;
+//let angka2 = 9;
+
+//console. log(angka1 <= angka2);
+
+//ternary operator
+//let angka = 5;
+//let hasil = angka <= 0 ? "Negatif" : "Positif";
+//console. log(hasil);
+
+//Operator Logika
+//let kondisi1 =1 <2;
+//let kondisi2 = true;
+
+//console. log (kondisi1 || kondisi2);
+
+//console. log(!kondisi1);
+
+//let angka = prompt("Masukan angka : ");
+//console. log ("angka yang dimasukan adalah " + angka);
+
+//Exercise #7
+/*
+var tinggi,berat,keterangan,bmi;
+function hitung() {
+	tinggi = parseFloat(document.getElementById("ipt_t").value);
+	tinggi /= 100;
+	berat = parseFloat(document.getElementById("ipt_b").value);
+	bmi = berat / (tinggi * tinggi);
+ 
+	if (bmi > 27) {
+		keterangan = "Gemuk, Kelebihan berat badan tingkat berat";
+	}else if ((bmi >= 25.1) & (bmi <= 27)){
+		keterangan = "Gemuk, Kelebihan berat badan tingkat ringan";
+	}else if ((bmi >= 18.5) & (bmi <= 25)){
+		keterangan = "Normal";
+	} else if ((bmi >= 17) & (bmi <= 18.4)){
+		keterangan = "Kurus, Kekurangan berat badan tingkat ringan";
+	}else {
+		keterangan = "Kurus, Kekurangan berat badan tingkat berat";
+	}
+	document.getElementById('bmi').innerHTML = "Hasil perhitungan BMI : " + bmi.toFixed(1);
+	document.getElementById('keterangan').innerHTML = keterangan;
+}
+*/
+
+
+
